d8a4ff08d8fc50b703ac6e13ec439954a1670522
gagan-28deep <gagan_28_deep@outlook.com>
2024-02-25 22:28:16 +0530

    user controllers for update data

---
+++ src/models/subscription.model.js
@@ -0,0 +1,19 @@
+import mongoose, { Schema } from "mongoose";
+
+const subscriptionSchema = new Schema(
+  {
+    subscriber: {
+      // One who is subscribing
+      type: Schema.Types.ObjectId(),
+      ref: "User",
+    },
+    channle: {
+      // One to whom 'subscriber' is subscribing
+      type: Schema.Types.ObjectId(),
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+export const Subscription = mongoose.model("Subscription", subscriptionSchema);
--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -1,18 +1,18 @@
-// class ApiResponse {
-//   constructor(statusCode, data, message = "Success") {
-//       this.statusCode = statusCode;
-//       (this.data = data),
-//       (this.message = message),
-//       this.statusCode < 400;
-//   }
-// }
-
 class ApiResponse {
   constructor(statusCode, data, message = "Success") {
-    this.statusCode = statusCode;
-    this.data = data;
-    this.message = message;
+      this.statusCode = statusCode;
+      (this.data = data),
+      (this.message = message),
+      this.statusCode < 400;
   }
 }
+
+// class ApiResponse {
+//   constructor(statusCode, data, message = "Success") {
+//     this.statusCode = statusCode;
+//     this.data = data;
+//     this.message = message;
+//   }
+// }
 
 export { ApiResponse };
