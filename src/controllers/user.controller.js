e3b2a9f4313b9a737e146cc6132b1cbcce527387
gagan-28deep <gagan_28_deep@outlook.com>
2024-02-26 14:08:24 +0530

    logout user updated

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -162,9 +162,14 @@ const loginUser = asyncHandler(async (req, res) => {
 const logOutUser = asyncHandler(async (req, res) => {
   await User.findByIdAndUpdate(
     req.user?._id,
+    // {
+    //   $set: {
+    //     refreshToken: undefined,
+    //   },
+    // },
     {
-      $set: {
-        refreshToken: undefined,
+      $unset: {
+        refreshToken: 1,
       },
     },
     { new: true }
